e8d7921a019631341ecfd11e5d3c3ed34b9c243f
aladdineng <aladdin.enbeh@gmail.com>
2024-05-02 19:57:01 +0300

    array Function

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -10,7 +10,14 @@
  */
 function isArrayLengthOdd(numbers) {
   // Your code here
+
+  if (numbers.length % 2 == 1) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
+isArrayLengthOdd([1, 2, 3]);
 
 /**
  * isArrayLengthEven(numbers):
@@ -24,7 +31,13 @@ function isArrayLengthOdd(numbers) {
  */
 function isArrayLengthEven(numbers) {
   // Your code here
+  if (numbers.length % 0 == 2) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
+isArrayLengthEven([2, 3, 4]);
 
 /**
  * addLailaToArray(instructors):
@@ -36,7 +49,12 @@ function isArrayLengthEven(numbers) {
  */
 function addLailaToArray(instructors) {
   // Your code here
+
+  instructors.push("Laila");
+  return instructors;
 }
+let names = ["Mshary", "Hasan"];
+console.log(addLailaToArray(names));
 
 /**
  * eliminateTeam(teams):
@@ -48,8 +66,11 @@ function addLailaToArray(instructors) {
  */
 function eliminateTeam(teams) {
   // Your code here
+  teams.pop();
+  return teams;
 }
-
+let teams = ["Brazil", "Germany", "Italy"];
+console.log(eliminateTeam(teams));
 /**
  * secondHalfOfArrayIfItIsEven(fruits):
  * - receives array `fruits`
@@ -62,8 +83,23 @@ function eliminateTeam(teams) {
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
   // Your code here
+  const secondHalfOfArrayIfItIsEven = fruits;
+  if (fruits.length % 2 == 0) {
+    return fruits.slice(fruits.length / 2);
+  } else {
+    return [];
+  }
 }
-
+console.log(secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]));
+console.log(
+  secondHalfOfArrayIfItIsEven([
+    "apple",
+    "orange",
+    "banana",
+    "kiwi",
+    "blueberry",
+  ])
+);
 /**
  * youGottaCalmDown(shout):
  * - receives a string `shout`
@@ -80,4 +116,13 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  */
 function youGottaCalmDown(shout) {
   // Your code here
+  const firstindex = shout.indexOf("!");
+  if (firstindex !== -1) {
+    return shout.slice(0, firstindex + 1);
+  } else {
+    return shout;
+  }
 }
+console.log(youGottaCalmDown("HI!!!!!!!!!!"));
+console.log(youGottaCalmDown("Taylor Schwifting!!!!!!!!!!!"));
+console.log(youGottaCalmDown("Hellooooo"));
